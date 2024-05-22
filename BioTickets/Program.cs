90e2399910b05990581c23ef2579f489d35db55c
KikiBerg <153670000+KikiBerg@users.noreply.github.com>
2024-05-22 14:53:29 +0200

    Add meny alternativ

--- BioTickets/Program.cs
+++ BioTickets/Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hej, detta är ett test meddelande inför första push");
+            
+
+            bool programIsRunning = true; // Variabel som styr om programmet ska fortsätta köra
+            while (programIsRunning) // Oändlig loop för att hålla programmet vid liv tills användaren väljer att avsluta
+            {
+                Console.WriteLine("Välkommen till huvudmenyn! Navigera genom att skriva in siffror för att testa olika funktioner.");
+                Console.WriteLine("Välj ett av följande alternativ");
+                Console.WriteLine("0. Avsluta programmet");
+                Console.WriteLine("1. Ungdom eller pensionär");
+                Console.WriteLine("2. Beräkna pris för ett sällskap");
+                Console.WriteLine("3. Upprepa tio gånger");
+                Console.WriteLine("4. Det tredje ordet");
+
+            }
+
+
         }
     }
 }
