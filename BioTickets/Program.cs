2cd5df539271ce89001132b063737f70ad2e97de
KikiBerg <153670000+KikiBerg@users.noreply.github.com>
2024-05-22 10:15:57 +0200

    Add project files.

---
+++ BioTickets/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace BioTickets
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hej, detta är ett test meddelande inför första push");
+        }
+    }
+}
