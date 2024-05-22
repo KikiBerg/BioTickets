96fef520865c38dd3b7c4f482c9ea36e772b93d6
KikiBerg <153670000+KikiBerg@users.noreply.github.com>
2024-05-22 17:06:27 +0200

    Add class JuniorOrSenior för case 1.

--- BioTickets/Program.cs
+++ BioTickets/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            
+
 
             bool programIsRunning = true; // Variabel som styr om programmet ska fortsätta köra
             while (programIsRunning) // Oändlig loop för att hålla programmet vid liv tills användaren väljer att avsluta
@@ -18,40 +18,66 @@
                 Console.WriteLine("4. Det tredje ordet");
 
                 Console.Write("Vänligen ange ditt val: ");
-                // Lägg till extra validering för använderns input
-                // Försöker konvertera användarens input till en integer
-                // Om det misslyckas, informera användaren och fortsätt loopen
-                if (int.TryParse(Console.ReadLine(), out int input)) 
+                string input = Console.ReadLine();
+
+                switch (input)
                 {
-                    switch (input)
-                    {
-                        case "0":
-                            Console.WriteLine("Programmet avslutas");
-                            programIsRunning = false; // loopen stoppas här och programmet avslutas 
-                            break;
-                        case "1":
-                            // Kod för "Ungdom eller pensionär"
-                            break;
-                        case "2":
-                            // Kod för "Beräkna pris för ett sällskap"
-                            break;
-                        case "3":
-                            // Kod för "Upprepa tio gånger"
-                            break;
-                        case "4":
-                            // Kod för "Det tredje ordet"
-                            break;
-                        default:
-                            Console.WriteLine("Din input var felaktig, försök igen!");
-                            break;
-                    }
-                }
-                else
-                {
-                    Console.WriteLine("Din input var inte rätt, ange enbart en siffra mellan 0-4.");
+                    case "0":
+                        Console.WriteLine("Programmet avslutas");
+                        programIsRunning = false; // loopen stoppas här och programmet avslutas 
+                        break;
+                    case "1":
+                        // Kod för "Ungdom eller pensionär"
+                        break;
+                    case "2":
+                        // Kod för "Beräkna pris för ett sällskap"
+                        break;
+                    case "3":
+                        // Kod för "Upprepa tio gånger"
+                        break;
+                    case "4":
+                        // Kod för "Det tredje ordet"
+                        break;
+                    default:
+                        Console.WriteLine("Din input var felaktig, försök igen och ange enbart en siffra mellan 0-4.");
+                        break;
                 }
             }
         }
+
+        static void JuniorOrSenior() 
+        {
+            Console.WriteLine("Ange din ålder: ");
+            string ageInput = Console.ReadLine();
+
+            if (int.TryParse(ageInput, out int age)) 
+            { 
+                if (age < 5 || age < 100) 
+                {
+                    Console.WriteLine("Du får gratis inträde!");
+                }
+                else if (age < 20) 
+                {
+                    Console.WriteLine("Ungdomspris: 80kr");
+                }
+                else if (age > 64)
+                {
+                    Console.WriteLine("Pensionärspris: 90kr");
+                }
+                else
+                {
+                    Console.WriteLine("Standardpris: 120kr");
+                }
+            }
+            else 
+            {
+                Console.WriteLine("Felaktig ålder, försök igen.");
+            }
+            
+        }
+
+
+
 
 
     }
