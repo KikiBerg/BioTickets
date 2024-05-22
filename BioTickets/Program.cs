ee41157dfd64fb32b22431557788b22f78f7d209
KikiBerg <153670000+KikiBerg@users.noreply.github.com>
2024-05-22 16:11:40 +0200

    Lägg till extra validering med if-else utanför switchen.

--- BioTickets/Program.cs
+++ BioTickets/Program.cs
@@ -18,34 +18,45 @@
                 Console.WriteLine("4. Det tredje ordet");
 
                 Console.Write("Vänligen ange ditt val: ");
-                string input = Console.ReadLine();
-
-                switch (input) 
-                { 
-                    case "0":
-                        Console.WriteLine("Programmet avslutas"); 
-                        programIsRunning = false; // loopen stoppas här och programmet avslutas 
-                        break;
-                    case "1":
-                        // Kod för "Ungdom eller pensionär"
-                        break;
-                    case "2":
-                        // Kod för "Beräkna pris för ett sällskap"
-                        break;
-                    case "3":
-                        // Kod för "Upprepa tio gånger"
-                        break;
-                    case "4":
-                        // Kod för "Det tredje ordet"
-                        break;
-                    default:
-                        Console.WriteLine("Din input var inte rätt, försök igen och ange enbart en siffra mellan 0-4.");
-                        break;
+                // Lägg till extra validering för använderns input
+                // Försöker konvertera användarens input till en integer
+                // Om det misslyckas, informera användaren och fortsätt loopen
+                if (int.TryParse(Console.ReadLine(), out int input)) 
+                {
+                    switch (input)
+                    {
+                        case "0":
+                            Console.WriteLine("Programmet avslutas");
+                            programIsRunning = false; // loopen stoppas här och programmet avslutas 
+                            break;
+                        case "1":
+                            // Kod för "Ungdom eller pensionär"
+                            break;
+                        case "2":
+                            // Kod för "Beräkna pris för ett sällskap"
+                            break;
+                        case "3":
+                            // Kod för "Upprepa tio gånger"
+                            break;
+                        case "4":
+                            // Kod för "Det tredje ordet"
+                            break;
+                        default:
+                            Console.WriteLine("Din input var felaktig, försök igen!");
+                            break;
+                    }
                 }
-
+                else
+                {
+                    Console.WriteLine("Din input var inte rätt, ange enbart en siffra mellan 0-4.");
+                }
             }
-
-
         }
+
+
     }
 }
+
+
+
+
